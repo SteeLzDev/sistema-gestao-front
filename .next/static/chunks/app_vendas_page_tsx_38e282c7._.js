8c0bf45743c702d9d95b548305df8b282c3695f4
unknown <davi.silva21@yahoo.com>
2025-05-07 23:43:28 -0300

    Finalizado configurações de telas para autenticação de JWT

---
+++ .next/static/chunks/app_vendas_page_tsx_38e282c7._.js
@@ -0,0 +1,12 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/app_vendas_page_tsx_38e282c7._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/app_vendas_page_tsx_38e282c7._.js",
+  "chunks": [
+    "static/chunks/[turbopack]_browser_dev_hmr-client_hmr-client_ts_4257750b._.js",
+    "static/chunks/_15f9529d._.js"
+  ],
+  "source": "dynamic"
+});
